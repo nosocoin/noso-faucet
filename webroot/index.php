2d0a4de4e8dc27dbd679889043609bd733d22669
pasichDev <apasichnik9@gmail.com>
2022-02-07 23:13:17 +0200

    newRouter

--- webroot/index.php
+++ webroot/index.php
@@ -8,6 +8,10 @@ include dirname(__DIR__).'/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(dirname(__DIR__).'/config');
 $dotenv->load();
 
+//Check empty env
+$dotenv->required(['NOSO_PAY','CLAIM_TIME','MIN_NOSO_PAYMENTS','PERCENT_REF'])->notEmpty();
+
+
 //Enable routing
 require __DIR__ . '/../src/routes.php';
 
