b8977b36062363026aea738686f851332ee8e3ef
pasichDev <apasichnik9@gmail.com>
2022-02-08 16:42:00 +0200

    SwitchToSlimPHP

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -2,11 +2,14 @@
 	
 	namespace NosoProject\Controllers;
 
+
 	use \Psr\Http\Message\ServerRequestInterface as Request;
 	use \Psr\Http\Message\ResponseInterface as Response;
 	
 	class AuthController extends Controller
 	{
+
+       
 		
 		public function index(Request $request, Response $response)
 		{
