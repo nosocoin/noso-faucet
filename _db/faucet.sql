96896b4d32cff2d46c32bf8b7bceeeb0fdc1bb7d
pasichDev <apasichnik9@gmail.com>
2022-02-10 19:56:45 +0200

    add MySql

--- _db/faucet.sql
+++ _db/faucet.sql
@@ -1,27 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Фев 02 2022 г., 22:30
--- Версия сервера: 8.0.24
--- Версия PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `faucet`
---
-
--- --------------------------------------------------------
 
 --
 -- Структура таблицы `claim`
