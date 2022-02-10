6deeab22e7eeed9d887d3f54cebb1dcc5e029c80
pasichDev <apasichnik9@gmail.com>
2022-02-10 16:11:51 +0200

    check

--- src/Controllers/FaucetController.php
+++ src/Controllers/FaucetController.php
@@ -5,20 +5,20 @@
 	use \Psr\Http\Message\ResponseInterface as Response;
 	use NosoProject\Model\FaucetModel;
     
-	final class FaucetController {
-	    protected $container;
-		protected $FaucetModel;
+final class FaucetController {
+	protected $container;
+	protected $FaucetModel;
 
-	    public function __construct($container){
-		    $this->container = $container;
-			$this->FaucetModel = new FaucetModel($container->get('db'));
+	public function __construct($container){
+		$this->container = $container;
+		$this->FaucetModel = new FaucetModel($container->get('db'));
 	     
 	
-	    }
-
-	    public function index(Request $request, Response $response){
-			return $this->container->view->render($response, 'faucet.twig', $this->FaucetModel->OptionsArray());
-	
-			}
-		
 	}
+
+	public function index(Request $request, Response $response){
+		return $this->container->view->render($response, 'faucet.twig', $this->FaucetModel->OptionsArray());
+	
+	}
+		
+}
