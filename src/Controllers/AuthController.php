a52c7c76cd6d85f697bd741eb450d88bc0249216
pasichDev <apasichnik9@gmail.com>
2022-02-12 23:18:40 +0200

    add new array

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -5,7 +5,13 @@
 	use \Psr\Http\Message\ResponseInterface as Response;
 	use NosoProject\Model\AuthModel;
 
-	
+	use Dflydev\FigCookies\Modifier\SameSite;
+    use Dflydev\FigCookies\SetCookie;
+	use Dflydev\FigCookies\FigResponseCookies;
+	use Dflydev\FigCookies\FigRequestCookies;
+
+	use Carbon\Carbon;
+	use NosoProject\Core\Cookies;
 final class AuthController  {
 
 	protected $container;
@@ -14,16 +20,31 @@ final class AuthController  {
 	public function __construct($container){
 		$this->container = $container;
 		$this->AuthModel = new AuthModel($container);
-	
 	}
 
+
+
 	public function index(Request $request, Response $response){
+
+
+	
+
+		$cookie = FigRequestCookies::get($request,'theme', 'default');
 			return $this->container->view->render($response, 'auth.twig',
-			$this->AuthModel->OptionArray(false));
+			 [
+				'title' => 'Authorization',
+				'wall' => Cookies::get($request, 'name')
+			   
+			]	);
 	}
 		
 	public function login(Request $request, Response $response){
 			if($this->AuthModel->routeAuth()){
+			
+			
+		
+				Cookies::add($response, 'name', 'value', 1, 'month');
+
 				return $response->withStatus(302)->withHeader('Location', '/');
 			}else{
 				return $this->container->view->render($response, 'auth.twig', 
