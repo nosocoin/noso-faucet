084de3561b5d525be75744d58e6c7ab66b7fbaa3
pasichDev <apasichnik9@gmail.com>
2022-02-19 14:49:00 +0200

    add statistic

--- src/Model/PaymentsModel.php
+++ src/Model/PaymentsModel.php
@@ -22,11 +22,31 @@ final class PaymentsModel
       'ViewPayments' => false,
       'MinimymPayNoso' => $_ENV['MIN_NOSO_PAYMENTS'],
       'WalletUser' => $this->UserArray['wallet'],
+      'ArrayClaims' => $this->GetArrayClaims(),
+      'ArrayPayments' => $this->GetArrayPayments()
 
 
     ];
   }
 
+
+  /**
+   * This method returns the last 8 claims in an array
+   */
+  private function GetArrayClaims(){
+    $claim = $this->DB->prepare("SELECT * FROM `claim` WHERE `wallet`= :wallet ORDER BY `date` DESC LIMIT 8");
+    $claim->execute(array('wallet' => $this->UserArray['wallet']));
+   return $claim->fetchAll(\PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * This method returns an array of data about the latest payments to the wallet!
+   */
+  private function GetArrayPayments(){
+    $paymnets = $this->DB->prepare("SELECT * FROM `payments` WHERE `wallet`= :wallet ORDER BY `date` DESC ");
+    $paymnets->execute(array('wallet' => $this->UserArray['wallet']));
+   return $paymnets->fetchAll(\PDO::FETCH_ASSOC);
+  }
 
 
 
