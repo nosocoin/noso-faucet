b1c09e642887d34d327ae7558666aa9ba706affa
pasichDev <apasichnik9@gmail.com>
2022-02-19 14:52:38 +0200

    formatting

--- src/Model/PaymentsModel.php
+++ src/Model/PaymentsModel.php
@@ -11,7 +11,6 @@ final class PaymentsModel
   {
     $this->UserArray = $container->get('UserAuthInfo');
     $this->DB = $container->get('db');
-    
   }
 
 
@@ -24,8 +23,6 @@ final class PaymentsModel
       'WalletUser' => $this->UserArray['wallet'],
       'ArrayClaims' => $this->GetArrayClaims(),
       'ArrayPayments' => $this->GetArrayPayments()
-
-
     ];
   }
 
@@ -33,22 +30,20 @@ final class PaymentsModel
   /**
    * This method returns the last 8 claims in an array
    */
-  private function GetArrayClaims(){
+  private function GetArrayClaims()
+  {
     $claim = $this->DB->prepare("SELECT * FROM `claim` WHERE `wallet`= :wallet ORDER BY `date` DESC LIMIT 8");
     $claim->execute(array('wallet' => $this->UserArray['wallet']));
-   return $claim->fetchAll(\PDO::FETCH_ASSOC);
+    return $claim->fetchAll(\PDO::FETCH_ASSOC);
   }
 
   /**
    * This method returns an array of data about the latest payments to the wallet!
    */
-  private function GetArrayPayments(){
+  private function GetArrayPayments()
+  {
     $paymnets = $this->DB->prepare("SELECT * FROM `payments` WHERE `wallet`= :wallet ORDER BY `date` DESC ");
     $paymnets->execute(array('wallet' => $this->UserArray['wallet']));
-   return $paymnets->fetchAll(\PDO::FETCH_ASSOC);
+    return $paymnets->fetchAll(\PDO::FETCH_ASSOC);
   }
-
-
-
 }
-
